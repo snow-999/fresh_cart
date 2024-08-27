caf8ad464357ab1148933adf8d227c0ba8fe1b3c
khaled <khaledleader012@gmail.com>
2024-08-27 19:06:06 +0300

    msg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}' , './index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
     extend: {
-      container:{
-        center:true
+      container: {
+        center: true
       }
     },
   },
